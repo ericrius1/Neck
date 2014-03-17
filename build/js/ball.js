fd033f1534916c184bb1df2007970b3da7b838c6
Eric <Ericrius1@gmail.com>
2014-03-17 12:13:13 -0500

    adding stars around galay

--- build/js/ball.js
+++ build/js/ball.js
@@ -2,14 +2,15 @@ var Ball;
 
 FW.Ball = Ball = (function() {
   function Ball(position) {
-    var ballMaterial, color, sphereGeometry;
+    var ballMat, ballMaterial, color, sphereGeometry;
     this.position = position;
     sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
     color = new THREE.Color();
     color.setRGB(rnd(0, 1), rnd(0, 1), rnd(0, 1));
-    ballMaterial = Physijs.createMaterial(new THREE.MeshBasicMaterial({
-      color: color
-    }), -1.0, 1.0);
+    ballMat = new THREE.MeshBasicMaterial({
+      map: THREE.ImageUtils.loadTexture('assets/photos/galaxy.jpg')
+    });
+    ballMaterial = Physijs.createMaterial(ballMat, -1.0, 1.0);
     this.ball = new Physijs.SphereMesh(sphereGeometry, ballMaterial, void 0);
     this.ball.position = this.position;
     FW.scene.add(this.ball);
--- src/ball.coffee
+++ src/ball.coffee
@@ -3,8 +3,9 @@ FW.Ball= class Ball
     sphereGeometry = new THREE.SphereGeometry 2, 32, 32
     color = new THREE.Color()
     color.setRGB rnd(0,1), rnd(0,1), rnd(0,1)
+    ballMat = new THREE.MeshBasicMaterial map: THREE.ImageUtils.loadTexture('assets/photos/galaxy.jpg')
     ballMaterial = Physijs.createMaterial \
-      new THREE.MeshBasicMaterial(color: color)
+      ballMat
       , -1.0 # low friction
       , 1.0 #bouncy
     @ball = new Physijs.SphereMesh sphereGeometry, ballMaterial, undefined
